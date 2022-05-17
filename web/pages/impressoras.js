9ef811248153a20afb7e11d972664c423abc47ae
Luiz Carlos <nonam4@yahoo.com>
2022-05-17 08:52:02 -0300

    voltando para antes das alterações

--- web/pages/impressoras.js
+++ web/pages/impressoras.js
@@ -61,8 +61,6 @@ function Impressoras () {
 
     // se o campo de busca ou os dados dos cadastros mudarem o sistema vai:
     useEffect( () => {
-        if ( !dadosSolicitados ) return
-
         // se a data dos filtros mudar e o cadastro de impressoras estiver em stack, atualiza a url
         if ( router.query.data !== filtros.data && router.query.stack === 'cadastroimpressoras' ) {
             let paginaAtual = router.pathname.replace( '/', '' )
