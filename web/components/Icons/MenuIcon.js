d0f5cd39ca292eda9f62536e165ee81a6dc3859a
Luiz Carlos <nonam4@yahoo.com>
2022-06-26 10:44:52 -0300

    refatoração de código

--- web/components/Icons/MenuIcon.js
+++ web/components/Icons/MenuIcon.js
@@ -1,19 +1,19 @@
 import { useContext } from 'react'
-import styled, { ThemeContext } from "styled-components"
+import styled, { ThemeContext } from 'styled-components'
 import icons from './index'
 
-const Svg = styled.svg.attrs({ 
-    version: '1.1', 
-    xmlns: 'http://www.w3.org/2000/svg', 
+const Svg = styled.svg.attrs( {
+    version: '1.1',
+    xmlns: 'http://www.w3.org/2000/svg',
     xmlnsXlink: 'http://www.w3.org/1999/xlink',
-})`
-    width: ${({ width }) => width}px;
-    height: ${({ height }) => height}px;
-    margin-right: ${({ margin }) => margin}rem;
+} )`
+    width: ${ ( { width } ) => width }px;
+    height: ${ ( { height } ) => height }px;
+    margin-right: ${ ( { margin } ) => margin }rem;
 `
 
-function UnderlineIcon(props) {
-    const { colors } = useContext(ThemeContext)
+function UnderlineIcon ( props ) {
+    const { colors } = useContext( ThemeContext )
     const settings = {
         size: 20,
         margin: '0.8',
@@ -21,10 +21,10 @@ function UnderlineIcon(props) {
         name: 'undefined'
     }
     return (
-        <Svg viewBox="0 0 200 200" height={props.size || settings.size} width={props.size || settings.size} 
-            margin={props.margin || settings.margin}  onClick={props.onClick}>
-            <path fill={props.color || settings.color} d={icons[props.name || settings.name]}/>
-            {props.title && <title>{props.title}</title>}
+        <Svg viewBox="0 0 200 200" height={ props.size || settings.size } width={ props.size || settings.size }
+            margin={ props.margin || settings.margin } onClick={ props.onClick }>
+            <path fill={ props.color || settings.color } d={ icons[ props.name || settings.name ] } />
+            { props.title && <title>{ props.title }</title> }
         </Svg>
     )
 }
