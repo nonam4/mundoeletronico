277cdef609132a81b40953d44d8aae04414b54a4
Luiz Carlos <nonam4@yahoo.com>
2022-07-01 14:58:48 -0300

    reformulando a clonagem de DB

--- web/pages/api/clonedb.js
+++ web/pages/api/clonedb.js
@@ -4,32 +4,33 @@ import axios from 'axios'
 export default async ( req, res ) => {
 
     let batch = database.batch()
-    await axios.request( 'https://us-central1-ioi-printers.cloudfunctions.net/dados', {
-        params: {
-            plataforma: 'web',
-            usuario: process.env.USER,
-            senha: process.env.PASS
-        }
-    } ).then( async res => {
+    await axios.request( 'https://us-central1-ioi-printers.cloudfunctions.net/dados',
+        { params: { plataforma: 'web', usuario: process.env.USER, senha: process.env.PASS } }
+    ).then( async res => {
 
-        let old = res.data.clientes
-        for ( let id in old ) {
-            let velho = old[ id ]
+        let clientes = res.data.clientes
+        for ( let id in clientes ) {
+
+            let velho = clientes[ id ]
+            let cadastro = {}
+
+            cadastro.id = id
+            cadastro.ativo = velho.ativo
+            cadastro.contato = velho.contato
+            cadastro.cpfcnpj = velho.cpfcnpj
+            cadastro.endereco = velho.endereco
+            cadastro.nomefantasia = velho.nomefantasia
+            cadastro.razaosocial = velho.razaosocial
+
+            // se o cadastro velho não estiver mais ativo
+            if ( !velho.ativo ) batch.set( database.doc( `/cadastros/${ id }` ), cadastro )
             if ( !velho.ativo ) continue
-            let cliente = {}
-            let franquia = {}
 
-            cliente.id = id
-            cliente.ativo = velho.ativo
-            cliente.tipo = 'locacao'
-            cliente.contato = velho.contato
-            cliente.cpfcnpj = velho.cpfcnpj
-            cliente.endereco = velho.endereco
-            cliente.nomefantasia = velho.nomefantasia
-            cliente.razaosocial = velho.razaosocial
-            cliente.sistema = velho.sistema
+            cadastro.tipo = 'locacao'
+            cadastro.sistema = velho.sistema
 
-            cliente.horarios = { segunda: { aberto: false }, terca: { aberto: false }, quarta: { aberto: false }, quinta: { aberto: false }, sexta: { aberto: false }, sabado: { aberto: false } }
+            // define os horários para o novo sistema com base nos horários antigos cadastrados
+            cadastro.horarios = { segunda: { aberto: false }, terca: { aberto: false }, quarta: { aberto: false }, quinta: { aberto: false }, sexta: { aberto: false }, sabado: { aberto: false } }
             for ( let dia in velho.horarios ) {
                 let horario = velho.horarios[ dia ]
                 let horarios = horario.horario
@@ -40,62 +41,62 @@ export default async ( req, res ) => {
                 let a = horarios[ 0 ].split( ' - ' )
                 let b = horarios[ 1 ].split( ' - ' )
 
-                cliente.horarios[ dia ] = {
+                cadastro.horarios[ dia ] = {
                     aberto: horario.aberto,
                     horarios: { 0: a[ 0 ], 1: a[ 1 ], 2: b[ 0 ], 3: b[ 1 ] }
                 }
             }
 
-            franquia.limite = velho.franquia.valor || 0
-            franquia.tipo = velho.franquia.tipo || 'ilimitado'
-            franquia.vpe = velho.franquia.preco || 0
-            cliente.franquia = franquia
+            // atualiza as franquias para o novo formato
+            cadastro.franquia = {
+                limite: velho.franquia.valor || 0,
+                tipo: velho.franquia.tipo || 'ilimitado',
+                vpe: velho.franquia.preco || 0,
 
-            cliente.sistema.versao = 'N/I'
-            if ( String( cliente.sistema.versao ).toLowerCase() !== 'não instalado' ) cliente.sistema.versao = '0.1.0'
+            }
 
-            cliente.impresso = 0
-            cliente.excedentes = 0
+            cadastro.sistema.versao = 'N/I'
+            if ( String( cadastro.sistema.versao ).toLowerCase() !== 'não instalado' ) cadastro.sistema.versao = '0.1.0'
+
+            cadastro.impresso = 0
+            cadastro.excedentes = 0
 
             let impressoras = {}
-            if ( !velho.impressoras ) cliente.tipo = 'particular'
-            if ( velho.fornecedor ) cliente.tipo = 'fornecedor'
+            if ( velho.fornecedor ) cadastro.tipo = 'fornecedor'
+            if ( !velho.impressoras ) cadastro.tipo = 'particular'
 
             if ( velho.impressoras && Object.keys( velho.impressoras ).length > 0 ) {
                 for ( let serial in velho.impressoras ) {
 
                     let velha = velho.impressoras[ serial ]
-                    if ( !velha.ativa ) continue
                     let impressora = {}
-                    let franquia = {}
 
-                    impressora.contabilizar = velha.ativa
-
-                    if ( serial.replace( /\(|\)|\-|\s/g, '' ) !== '' ) impressora.serial = serial.replace( /\(|\)|\-|\s/g, '' )
-                    if ( serial.replace( /\(|\)|\-|\s/g, '' ) === '' ) impressora.serial = serial
-                    impressora.ip = velha.ip
+                    // limpa o serial mas tenha certeza que não será passado nenhum serial vazio
+                    const replaced = serial.replace( /\(|\)|\-|\s/g, '' )
+                    impressora.serial = replaced === '' ? serial : replaced
                     impressora.modelo = velha.modelo
+                    impressora.ip = velha.ip
+                    impressora.contabilizar = velha.ativa
                     impressora.setor = velha.setor
                     impressora.substituida = velha.trocada || false
                     impressora.substituindo = []
                     if ( velha.substituindo ) impressora.substituindo.push( velha.substituindo )
 
-                    franquia.limite = velha.franquia || 0
-                    franquia.vpe = velho.franquia.preco || 0
-                    impressora.franquia = franquia
-
-                    impressora.tintas = {
-                        capacidade: 0,
-                        abastecido: 0
+                    impressora.franquia = {
+                        limite: velha.franquia || 0,
+                        vpe: velho.franquia.preco || 0
                     }
+
                     if ( velha.tinta ) impressora.tintas = {
-                        capacidade: velha.tinta.capacidade,
-                        abastecido: velha.tinta.cheio
+                        capacidade: velha.tinta.capacidade || 0,
+                        abastecido: velha.tinta.cheio || 0
                     }
 
                     impressora.contador = 0
                     impressora.contadores = {}
+
                     if ( velha.leituras && Object.keys( velha.leituras ).length > 0 ) {
+
                         for ( let key in velha.leituras ) {
                             let leitura = velha.leituras[ key ]
 
@@ -113,28 +114,15 @@ export default async ( req, res ) => {
                             }
 
                             contadores.impresso = leitura.final.valor - leitura.inicial.valor
-                            contadores.excedentes = 0
-                            if ( contadores.impresso > impressora.franquia.limite ) contadores.excedentes = contadores.impresso - impressora.franquia.limite
-                            switch ( cliente.franquia.tipo ) {
-                                case 'maquina':
-                                    cliente.excedentes += contadores.excedentes
-                                    break
-                                case 'pagina':
-                                    if ( contadores.impresso > cliente.franquia.limite ) cliente.excedentes += contadores.impresso - cliente.franquia.limite
-                                    break
-                                case 'ilimitado':
-                                    cliente.excedentes = contadores.impresso
-                                    break
-                            }
                             impressora.contadores[ key ] = contadores
                         }
                     }
                     impressoras[ impressora.serial ] = impressora
                 }
             }
-            cliente.impressoras = impressoras
-            //batch.set( database.doc( `/cadastros/${ cliente.id }` ), cliente, { merge: true } )
-            batch.set( database.doc( `/cadastros/${ cliente.id }` ), cliente )
+            cadastro.impressoras = impressoras
+            //batch.set( database.doc( `/cadastros/${ id }` ), cadastro, { merge: true } )
+            batch.set( database.doc( `/cadastros/${ id }` ), cadastro )
         }
         await batch.commit()
     } ).catch( err => {
